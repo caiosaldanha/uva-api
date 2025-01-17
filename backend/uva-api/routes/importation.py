2e59cdaf7527fb27206f3684aa5caa08a010e3a4
caiosaldanha <caiowrite@gmail.com>
2025-01-17 03:43:07 +0000

    project: unification of the solution

---
+++ backend/uva-api/routes/importation.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, HTTPException
+from services.vitibrasilscraper import VitibrasilScraper
+
+router = APIRouter()
+
+@router.get('/importation/{year}', summary='Importation')
+async def v1_get_importation(year):
+    try:
+        v1_get_importation_scraper = VitibrasilScraper(year=int(year))
+        v1_get_importation_df = v1_get_importation_scraper.get_importation()
+        return v1_get_importation_df
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f"Invalid value for year: {e}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An unexpected error occurred: {e}")
