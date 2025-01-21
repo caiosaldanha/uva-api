40d299e9e9a5b8294014570f1070bd113256e761
caiosaldanha <caiowrite@gmail.com>
2025-01-20 22:18:01 -0300

    testing ml models

--- backend/uva-api/routes/production.py
+++ backend/uva-api/routes/production.py
@@ -24,3 +24,15 @@ async def v1_get_production(year):
         raise HTTPException(status_code=400, detail=f"Invalid value for year: {e}")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An unexpected error occurred: {e}")
+    
+@router.post('/production/ml/{year}', summary='Production')
+async def v1_get_production(year):
+    try:
+        v1_get_production_scraper = VitibrasilScraper(year=int(year))
+        v1_get_production_df = v1_get_production_scraper.get_production()
+        v1_get_production_df['year'] = int(year)
+        return v1_get_production_df
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f"Invalid value for year: {e}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An unexpected error occurred: {e}")
